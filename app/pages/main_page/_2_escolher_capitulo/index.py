6c0eda13704df61f3809ffc7f6e14138d5bf783c
ruan fab <victor.ruanfab@gmail.com>
2025-02-25 23:32:01 -0300

    :sparkles: feat(capitulos): Atualiza o nome do capítulo e adiciona delays para melhorar a interação

--- app/pages/main_page/_2_escolher_capitulo/index.py
+++ app/pages/main_page/_2_escolher_capitulo/index.py
@@ -9,11 +9,14 @@ class Capitulos:
         print('escolha seu capitulo')
         
     def listaCapitulos(self):
+        time.sleep(1)
         botoes_auxiliar = self.driver.procurarArrayElementos('XPATH', "//*/div[@class='flex gap-x-2 mb-4']/button")[1]
         botoes_auxiliar.click()
         
         escolhendo_volume = self.driver.procurarElemento('XPATH', f"//*/h4[text()='Chapter {self.capitulo}']/../../../../../../../../../button")
         escolhendo_volume.click()
+        
+        time.sleep(2)
         
         pegando_capitulo = self.driver.procurarElemento('XPATH', f"//*/h4[text()='Chapter {self.capitulo}']/..")
         pegando_capitulo.click()
@@ -21,5 +24,5 @@ class Capitulos:
         escolhendo_idioma = self.driver.procurarElemento('XPATH', f"//*/h4[text()='Chapter {self.capitulo}']/../../../../../../../../..//*/img[@title='Portuguese (Br)']/../..")
         escolhendo_idioma.click()
         
-        print(f'esperando {self.capitulo}')
+        print(f'capitulo {self.capitulo}')
         time.sleep(348732)
